a02a522b622a43b3e64e482204d8fe2aa9c972a8
Antonina Maj <antoninajanina@gmail.com>
2019-02-05 16:26:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
     var tot = total();
-    
+    cart.splice(0);
+    return `Your total cost is $${tot}, which will be charged to the card ${cardNumber}.`;
   }
 }
