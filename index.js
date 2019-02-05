9612cc6972b583832affba25a8fd8d0e99783da8
Antonina Maj <antoninajanina@gmail.com>
2019-02-05 16:04:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,19 +10,44 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.floor(Math.random()*100);
+ var newObject = {itemName: item, itemPrice: price};
+ cart.push(newObject);
+ return `${item} has been added to your cart.`
 }
+
 
 function viewCart() {
-  // write your code here
+  var str = "In your cart, you have ";
+  
+  if (cart.length === 0) {
+    return "Your shopping cart is empty.";
+    
+  } else if (cart.length === 1) {
+    str = str + `${cart[0]['itemName']} at $${cart[0]['itemPrice']}.`;
+    return str;
+    
+  } else {
+    for (var i = 0; i < cart.length - 1; i++) {
+      str = str + `${cart[i]['itemName']} at $${cart[i]['itemPrice']}, `;
+    }
+    str = str + `and ${cart[cart.length - 1]['itemName']} at $${cart[cart.length - 1]['itemPrice']}.`;
+    return str;
+  }
 }
+
 
 function total() {
-  // write your code here
+  var tot = 0;
+  for (var i = 0; i < cart.length; i++) {
+    tot = tot + cart[i]['itemPrice'];
+  }
+  return tot;
 }
 
+
 function removeFromCart(item) {
-  // write your code here
+  
 }
 
 function placeOrder(cardNumber) {
