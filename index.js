bd2b4677bba30c8c96549598226e0888688a28e8
Antonina Maj <antoninajanina@gmail.com>
2019-02-05 16:09:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,9 +49,11 @@ function total() {
 function removeFromCart(item) {
   for (var i = 0; i<cart.length; i++) {
     if (cart[i]['itemName'] === item) {
-      
+      cart.splice(i,1);
+      return cart;
     }
   }
+  return 
 }
 
 
